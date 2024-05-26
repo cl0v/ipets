15e50c35d740834b9862ef816636f58d284d4012
Marcelo Viana <marcelofv12@hotmail.com>
2024-05-26 16:04:06 -0300

    new components

---
+++ components/PetImages.vue
@@ -0,0 +1,39 @@
+<template>
+    <!-- Image gallery -->
+    <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+        <div class="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
+            <img :src="imgs[0].src" :alt="imgs[0].alt" class="h-full w-full object-cover object-center" />
+        </div>
+        <div class="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+            <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+                <img :src="imgs[1].src" :alt="imgs[1].alt" class="h-full w-full object-cover object-center" />
+            </div>
+            <div class="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
+                <img :src="imgs[2].src" :alt="imgs[2].alt" class="h-full w-full object-cover object-center" />
+            </div>
+        </div>
+        <div class="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
+            <img :src="imgs[3].src" :alt="imgs[3].alt" class="h-full w-full object-cover object-center" />
+        </div>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+
+// const { images, setImages } = useImages()
+
+const { size, gender, color } = useRoute().query
+
+const { data, error } = await useFetch('/api/images', {
+    query: {
+        size: size,
+        gender: gender,
+        color: color
+    }
+},)
+
+// setImages(data.value!.images)
+
+const imgs = data.value!.images
+</script>
---
+++ components/PetPrice.vue
@@ -0,0 +1,20 @@
+<template>
+    <p class="text-3xl tracking-tight text-gray-900">{{ $t('global.currency') + price }}</p>
+</template>
+<script setup lang="ts">
+
+const { size, gender, color } = useRoute().query
+
+// Validar os valores das queries
+
+const { data } = await useFetch('/api/price', {
+    query: {
+        size: size,
+        gender: gender,
+        color: color
+    }
+})
+
+const price = data.value?.price
+
+</script>
