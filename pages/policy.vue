7b348088c477f9f1aca9e31573cecc9647627c4a
Marcelo Viana <marcelofv12@hotmail.com>
2024-05-27 22:08:41 -0300

    useSEOInstead

--- pages/policy.vue
+++ pages/policy.vue
@@ -1,25 +1,19 @@
 <script setup lang="ts">
 // Por algum motivo o SEO parece não estar funcionando.
-// useSeoMeta({
-//     title: 'iPets - Sua privacidade',
-//     ogTitle: 'iPets - Sua privacidade',
-//     description: 'Saiba como mantemos seus dados seguros na iPets.',
-//     ogDescription: 'Saiba como mantemos seus dados seguros na iPets.',
-//     ogImage: '/logo.png',
-// })
-useHead({
-    meta: [{ property: 'og:title', content: 'iPets - Sua privacidade' }, { property: 'og:description', content: 'Saiba como mantemos seus dados seguros na iPets.' },]
+useSeoMeta({
+    title: 'iPets - Sua privacidade',
+    ogTitle: 'iPets - Sua privacidade',
+    description: 'Saiba como mantemos seus dados seguros na iPets.',
+    ogDescription: 'Saiba como mantemos seus dados seguros na iPets.',
+    ogImage: '/logo.png',
 })
 </script>
-
 
 
 <template>
     <div>
         <div class="mx-32 my-8">
-            <h1>Saiba como mantemos seus dados seguros na iPets</h1>
-            <br>
-            <ContentDoc path="/lgpd" />
+            <ContentDoc path="/lgpd"/>
         </div>
     </div>
 </template>
