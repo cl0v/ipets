31db08bd42b36c26e261eb222b668e912303325a
Marcelo Viana <marcelofv12@hotmail.com>
2024-05-27 21:45:29 -0300

    Update policy.vue

--- pages/policy.vue
+++ pages/policy.vue
@@ -1,12 +1,26 @@
-<script setup>
+<!-- <script setup lang="ts">
 useSeoMeta({
     ogTitle: 'iPets - Sua privacidade',
     ogDescription: 'Saiba como mantemos seus dados seguros na iPets.'
 })
+</script> -->
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'iPets - Sua privacidade',
+  ogTitle: 'iPets - Sua privacidade',
+  description: 'Saiba como mantemos seus dados seguros na iPets.',
+  ogDescription: 'Saiba como mantemos seus dados seguros na iPets.',
+  ogImage: '/logo.png',
+//   twitterCard: 'summary_large_image',
+})
 </script>
+
 
 <template>
     <div>
-        <div class="mx-32 my-8"><ContentDoc path="/lgpd" /></div>
+        <div class="mx-32 my-8">
+            <ContentDoc path="/lgpd" />
+        </div>
     </div>
 </template>
