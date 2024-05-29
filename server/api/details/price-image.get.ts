18b6e4451bac9c6a0b5d1768801507f4d7ef26bb
Marcelo Viana <marcelofv12@hotmail.com>
2024-05-29 13:21:13 -0300

    price and image api in the same file

---
+++ server/api/details/price-image.get.ts
@@ -0,0 +1,20 @@
+import json from '~/assets/jsons/price_table.json'
+
+export default defineEventHandler((event) => {
+    const { breed, gender, size, color } = getQuery(event)
+
+    // const result = json.find((e) => e.uuid == breed && e.size == size && e.color == color)
+    const result = json.find((e) => e.uuid == breed)
+
+    const images = result!.images
+    let price = result!.price
+
+    if(gender == 'female'){
+        price *= 1.15
+    }
+
+    return {
+        images, 
+        price
+    }
+})
