597518966d31815b3bc4a3090846bd521a40dd8f
Marcelo Viana <marcelofv12@hotmail.com>
2024-05-30 18:20:48 -0300

    add tailwind module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,23 @@
 export default defineNuxtConfig({
   devtools: { enabled: true }, // Desativar em produção
   // css: ['~/assets/css/base.css'],
-  modules: ['@nuxt/ui', "@nuxtjs/i18n", "@nuxt/content",],
+  modules: ['@nuxt/ui', "@nuxtjs/i18n", // 'nuxt-stripe-module',
+  "@nuxt/content", "@nuxtjs/tailwindcss"],
   content: {
     // ... options
   },
   colorMode: {
     preference: 'light'
   },
+  // stripe: {
+  //   publishableKey: 'YOUR_STRIPE_PUBLISHABLE_KEY',
+  // },
   app: {
     head: {
       title: 'iPets - Filhotes disponíveis',
       charset: 'utf-8',
       meta: [
-        { name: 'description', content: 'Compre seu filhote de forma segura.'}
+        { name: 'description', content: 'Compre seu filhote de forma segura.' }
       ],
       viewport: 'width=device-width, initial-scale=1',
       link: [
