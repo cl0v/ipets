6c3a4613b39f97e6a7b59bbba2af5f06ab956ed6
Marcelo Viana <marcelofv12@hotmail.com>
2024-06-06 15:18:56 -0300

    rm service-accounts and fix firebaseconfig

--- server/utils/firebase.ts
+++ server/utils/firebase.ts
@@ -1,10 +1,29 @@
-import { initializeApp, cert } from 'firebase-admin/app';
+import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase-admin/firestore';
+// import { getAnalytics } from "firebase/analytics";
 
 
+// Import the functions you need from the SDKs you need
 
-export const app = initializeApp({
-    credential: cert('./service-account.json')
-})
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAMZf5xEsBpFZxOpVE3ualVETZSNuUXubk",
+  authDomain: "ipets-c854c.firebaseapp.com",
+  projectId: "ipets-c854c",
+  storageBucket: "ipets-c854c.appspot.com",
+  messagingSenderId: "926293712378",
+  appId: "1:926293712378:web:60762d7dc7486e91cb5843",
+  measurementId: "G-MX3J0MYSNQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// const analytics = getAnalytics(app);
 
 export const firestore = getFirestore(app);
