a5ca775a9c6b8707f6b8505e3deb3b0d351fa736
Marcelo Viana <marcelofv12@hotmail.com>
2024-06-06 21:57:33 -0300

    fix firebase

--- server/utils/firebase.ts
+++ server/utils/firebase.ts
@@ -16,24 +16,3 @@ export async function addOrderRef(data: Object): Promise<string> {
 
     return userDoc.id
 }
-
-
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase-admin/firestore";
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const config = {
-//     apiKey: "AIzaSyAMZf5xEsBpFZxOpVE3ualVETZSNuUXubk",
-//     authDomain: "ipets-c854c.firebaseapp.com",
-//     projectId: "ipets-c854c",
-//     storageBucket: "ipets-c854c.appspot.com",
-//     messagingSenderId: "926293712378",
-//     appId: "1:926293712378:web:60762d7dc7486e91cb5843",
-//     measurementId: "G-MX3J0MYSNQ"
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(config);
-
-// export const firestore = getFirestore(app);
