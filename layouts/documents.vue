68a35ed35a4882c7d8156eb3dcfbe9ec38f84a57
Marcelo Viana <marcelofv12@hotmail.com>
2024-06-11 11:59:35 -0300

    fix doc layout

--- layouts/documents.vue
+++ layouts/documents.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="lg:mx-32 my-8 sm:mx-12 max-sm:mx-4">
+        <div class="lg:mx-32 my-8 sm:mx-12 max-sm:mx-4 text-black">
             <slot />
         </div>
     </div>
