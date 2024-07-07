6f5eab673c8d968647875329a32afcee0ae9b4ff
Marcelo Viana <marcelofv12@hotmail.com>
2024-07-07 00:11:40 -0300

    add screen size reactivity

---
+++ TODO.md
@@ -0,0 +1,4 @@
+## Tasks
+* Melhorar o carrossel
+* Melhorar a navegacao por celular e dispositivos moveis
+* Adicionar um formato diferente pra celular e computador (Aproveitar o molde antigo pra computador)
--- components/DetailsImages.vue
+++ components/DetailsImages.vue
@@ -12,6 +12,18 @@ const { petImages } = usePetImages();
 
 
 <style>
+@media only screen and (max-width: 1026px) {
+    #fadeshow1 {
+        display: none;
+    }
+}
+
+@media only screen and (min-width: 1026px) {
+    #fadeshow2 {
+        display: none;
+    }
+}
+
 .carousel__item {
     /* 
     background-color: var(--vc-clr-primary);
@@ -32,28 +44,29 @@ const { petImages } = usePetImages();
 
 /* .carousel__prev, */
 /* .carousel__next { */
-    /* box-sizing: content-box; */
-    /* border: 5px solid white; */
+/* box-sizing: content-box; */
+/* border: 5px solid white; */
 /* }  */
-
 </style>
 
 <template>
     <ClientOnly>
-        <Carousel>
-            <Slide v-for="slide in petImages" :key="slide">
-                <div class="carousel__item">
-                    <NuxtImg :src="slide" alt="Imagem do filhote."
-                        class="h-full w-full object-cover object-center" />
-                </div>
-            </Slide>
-            <template #addons>
-                <Navigation />
-                <Pagination />
-            </template>
+        <div id="fadeshow2">
+            <Carousel>
+                <Slide v-for="slide in petImages" :key="slide">
+                    <div class="carousel__item">
+                        <NuxtImg :src="slide" alt="Imagem do filhote."
+                            class="h-full w-full object-cover object-center" />
+                    </div>
+                </Slide>
+                <template #addons>
+                    <Navigation />
+                    <Pagination />
+                </template>
 
-        </Carousel>
-        <!-- 
+            </Carousel>
+        </div>
+        <div id="fadeshow1">
             <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8"
                 data-carousel-item>
                 <div class="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
@@ -75,6 +88,7 @@ const { petImages } = usePetImages();
                     <NuxtImg :src="petImages[3]" alt="Quarta imagem do filhote."
                         class="h-full w-full object-cover object-center" />
                 </div>
-        </div> -->
+            </div>
+        </div>
     </ClientOnly>
 </template>
