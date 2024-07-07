a339040120b7ec8c2d91504503fc4fb43b179e21
Marcelo Viana <marcelofv12@hotmail.com>
2024-07-06 21:49:29 -0300

    add carrousel

--- components/DetailsImages.vue
+++ components/DetailsImages.vue
@@ -1,61 +1,58 @@
 <script setup lang="ts">
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
+import 'vue3-carousel/dist/carousel.css'
+
 import usePetImages from '~/composables/imagesState';
 
 const { petImages } = usePetImages();
 
 </script>
 
+
+<style>
+.carousel__item {
+    /* 
+    background-color: var(--vc-clr-primary);
+    color: var(--vc-clr-white);
+    font-size: 20px;
+    */
+    min-height: 200px;
+    width: 100%;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.carousel__slide {
+    padding: 10px;
+}
+
+/* .carousel__prev, */
+/* .carousel__next { */
+    /* box-sizing: content-box; */
+    /* border: 5px solid white; */
+/* }  */
+
+</style>
+
 <template>
     <ClientOnly>
-        <div id="controls-carousel" class="relative w-full" data-carousel="static">
-            <!-- Carousel wrapper -->
-            <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img :src="petImages[0]"
-                        class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        <Carousel>
+            <Slide v-for="slide in petImages" :key="slide">
+                <div class="carousel__item">
+                    <NuxtImg :src="slide" alt="Imagem do filhote."
+                        class="h-full w-full object-cover object-center" />
                 </div>
+            </Slide>
+            <template #addons>
+                <Navigation />
+                <Pagination />
+            </template>
 
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img :src="petImages[1]"
-                        class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                </div>
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img :src="petImages[2]"
-                        class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                </div>
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img :src="petImages[3]"
-                        class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                </div>
-            </div>
-            <!-- Slider controls -->
-            <button type="button"
-                class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                data-carousel-prev>
-                <span
-                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M5 1 1 5l4 4" />
-                    </svg>
-                    <span class="sr-only">Previous</span>
-                </span>
-            </button>
-            <button type="button"
-                class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                data-carousel-next>
-                <span
-                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 9 4-4-4-4" />
-                    </svg>
-                    <span class="sr-only">Next</span>
-                </span>
-            </button>
-        </div>
+        </Carousel>
         <!-- 
             <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8"
                 data-carousel-item>
